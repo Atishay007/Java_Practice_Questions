b1e881282bcfc94ff586cb51006cf7aa5dbfb590
Atishay Jain <atishayjain23@gmail.com>
2019-05-31 02:19:04 +0530

    Removed this pattern and moved into GitRepo

--- src/com/atishay/proxy/design/pattern/RealInternet.java
+++
@@ -1,9 +0,0 @@
-package com.atishay.proxy.design.pattern;
-
-public class RealInternet implements Internet {
-
-	@Override
-	public void connectTo(String serverHost) {
-		System.out.println("Connecting to: " + serverHost);
-	}
-}
