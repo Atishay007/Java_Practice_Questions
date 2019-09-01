d3a707e8b08478c4a532638551053d16014d2f48
Atishay Jain <atishayjain23@gmail.com>
2019-09-01 11:11:42 +0530

    Finding Abs Difference

--- src/com/atishay/practice/arrays/FindNoOfOddOccurrenceInArray.java
+++ src/com/atishay/practice/arrays/FindNoOfOddOccurrenceInArray.java
@@ -1,9 +1,11 @@
 package com.atishay.practice.arrays;
 
 //O(1) space and O(n) time complexity.
-public class FindNoOfOddOccurrenceFrom {
+//Using XOR
+public class FindNoOfOddOccurrenceInArray {
 	public static void main(String[] args) {
 		int[] arr = { 1, 1, 2, 2, 3, 3, 4 };
+		// Initialize this with 0.
 		int result = 0;
 
 		for (int i = 0; i < arr.length; i++) {
