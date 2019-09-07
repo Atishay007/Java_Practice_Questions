5e47cc1b41c47f8dd42d4b006a711ae7ab00099d
Atishay Jain <atishayjain23@gmail.com>
2019-09-07 17:09:38 +0530

    Renamed methods and removed extra files

--- src/com/atishay/practice/arrays/GetCountOfNonDuplicates.java
+++ src/com/atishay/practice/arrays/GetCountOfNonDuplicates.java
@@ -5,10 +5,10 @@ package com.atishay.practice.arrays;
 public class GetCountOfNonDuplicates {
 	public static void main(String[] args) {
 		int[] nums = { -1, -1, -1, -2, 0 };
-		System.out.println(removeDuplicates(nums));
+		System.out.println(getCountOfDuplicates(nums));
 	}
 
-	public static int removeDuplicates(int[] nums) {
+	public static int getCountOfDuplicates(int[] nums) {
 		// special case
 		if (nums.length == 0)
 			return 0;
@@ -18,7 +18,7 @@ public class GetCountOfNonDuplicates {
 		int uniqueNoCount = 0;
 
 		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != doBinarySearch(nums, i + 1, nums.length - 1, nums[i])) {
+			if (nums[i] != binarySearch(nums, i + 1, nums.length - 1, nums[i])) {
 				uniqueNoCount++;
 			}
 		}
@@ -26,15 +26,15 @@ public class GetCountOfNonDuplicates {
 	}
 
 	// Recursive Binary Search
-	private static int doBinarySearch(int[] nums, int start, int end, int noToFind) {
+	private static int binarySearch(int[] nums, int start, int end, int noToFind) {
 
 		if (start <= end) {
 			int pivot = (start + end) / 2;
 
 			if (nums[pivot] > noToFind) {
-				return doBinarySearch(nums, start, pivot - 1, noToFind);
+				return binarySearch(nums, start, pivot - 1, noToFind);
 			} else if (nums[pivot] < noToFind) {
-				return doBinarySearch(nums, pivot + 1, end, noToFind);
+				return binarySearch(nums, pivot + 1, end, noToFind);
 			} else {
 				int temp = nums[pivot];
 				nums[pivot] = -1;
