6d12c2b569a778617df46103fe7eea9b96d2b47d
Atishay Jain <atishayjain23@gmail.com>
2019-09-11 12:26:50 +0530

    First UniqueCharacter in String

---
+++ src/com/atishay/practice/arrays/FirstUniqueCharacterInString.java
@@ -0,0 +1,38 @@
+package com.atishay.practice.arrays;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Map.Entry;
+
+//RunTimeComplexity: O(n)
+//HashMap: Get and Put RumTimeComplexity O(1)
+public class FirstUniqueCharacterInString {
+
+	public static void main(String[] args) {
+		findFirstUniqueCharacter("ccdde");
+	}
+
+	private static void findFirstUniqueCharacter(String str) {
+		char[] arr = str.toCharArray();
+		Map<Character, Integer> map = new LinkedHashMap<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			// increment the count when you found the character.
+			if (map.get(arr[i]) != null) {
+				map.put(arr[i], map.get(arr[i]) + 1);
+			} else {
+				map.put(arr[i], 1);
+			}
+		}
+
+		Optional<Entry<Character, Integer>> opt = map.entrySet().stream().filter(entry -> entry.getValue() == 1)
+				.findFirst();
+		if (opt.isPresent()) {
+			System.out.println(opt.get().getKey());
+		} else {
+			System.out.println("-1");
+		}
+	}
+
+}
