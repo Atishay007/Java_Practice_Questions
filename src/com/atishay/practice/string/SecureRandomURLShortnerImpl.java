7373f61920afacf1765d9fd6a35a6071c9d9f04c
Atishay Jain <atishayjain23@gmail.com>
2019-12-23 09:36:17 +0530

    Java Practice Programs.

--- src/com/atishay/practice/string/SecureRandomURLShortnerImpl.java
+++ src/com/atishay/practice/string/SecureRandomURLShortnerImpl.java
@@ -1,6 +1,9 @@
-package com.atishay.miscellaneous;
+package com.atishay.practice.string;
 
 import java.security.SecureRandom;
+
+//Runtime Complexity: O(n)
+//Space Complexity: O(n)
 
 /*This problem was asked by Microsoft.
 
@@ -9,15 +12,15 @@ Implement a URL shortener with the following methods:
 shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
 restore(short), which expands the shortened string into the original URL. 
 If no such shortened string exists, return null.
-Hint: What if we enter the same URL twice?*/
-//Ans: For this we can store the URL as key in HashMap and its respective URL Shortner as a value.
-// We can check whether URL is same or not from HashMap and we can also check its value and
-//can have validations on them.
+Hint: What if we enter the same URL twice?
+Ans: For this we can store the URL as key in HashMap and its respective URL Shortner as a value.
+We can check whether URL is same or not from HashMap and we can also check its value and
+can have validations on them.
 
-//This will create a random 6 character alphanumeric String.
-//A basic core idea behind Building URL Shortner.
+This will create a random 6 character alphanumeric String.
+A basic core idea behind Building URL Shortner.
 
-//Article: https://www.baeldung.com/java-secure-random
+//Article: https://www.baeldung.com/java-secure-random*/
 public class SecureRandomURLShortnerImpl {
 	public static void main(String[] args) {
 		SecureRandom rnd = new SecureRandom();
