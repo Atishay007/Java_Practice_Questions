ff6c9a9e363a3000b4eb6c8b6913e2180fbbd679
Atishay <Atishay@192.168.2.3>
2019-12-25 20:49:22 +0530

    Proper explanation of Shift Operators.

--- src/com/atishay/miscellaneous/ShiftOperators.java
+++ src/com/atishay/miscellaneous/ShiftOperators.java
@@ -1,23 +1,32 @@
 package com.atishay.miscellaneous;
 
+/*
+ * 
+1. <<, Left shift operator: shifts the bits of the number to the left and fills 0 on voids left as a result.
+Similar effect as of multiplying the number with some power of two.
+
+2. >>, Signed Right shift operator: shifts the bits of the number to the right and fills 0 on voids left
+as a result. The leftmost bit depends on the sign of initial number. Similar effect as of dividing 
+the number with some power of two.
+
+3. >>>, Unsigned Right shift operator: shifts the bits of the number to the right and fills 0 on voids 
+left as a result. The leftmost bit is set to 0.
+
+*/
 public class ShiftOperators {
 	public static void main(String[] args) {
+		int a = 10;
 
-		System.out.println(Integer.toBinaryString(51));
-		// Left shift Operator Adds 0 in the end.
-		// In the below example, it will add 3 0's in the end.
-		System.out.println(Integer.toBinaryString(51 << 3));
-		System.out.println("No Present:" + (4 << 2));
+		// Left shift: 10 * (2^1): Means Multiplication
+		System.out.println(4 << 1); // result: 8
+		// Left shift: 10 * (2 ^2)
+		System.out.println(a << 1); // result: 4
 
-		System.out.println("Performing Right Shift");
-		System.out.println(Integer.toBinaryString(51));
-		// Right shift operator shift the bits to the right.
-		// Means it removes the bits from the last
-		// If you keep shifting the bits, there will be stage
-		// when it reaches to 0 and the output will be 0.
-		System.out.println(Integer.toBinaryString(51 >> 3));
-
-		System.out.println("Output will be 0");
-		System.out.println(Integer.toBinaryString(51 >> 7));
+		System.out.println("Right Shift results");
+		// Right shift means division (/)
+		// 10 / (2^1)
+		System.out.println(a >> 1); // result:
+		// 10 / (2^2)
+		System.out.println(a >> 2); // result:
 	}
 }
