98885e85fff9cc00c5a3e1b81e90608dd3403ba9
Atishay <Atishay@192.168.2.3>
2019-12-25 21:09:14 +0530

    Any Pair in Array whose sum is max.

---
+++ src/com/atishay/practice/arrays/AnyPairSumMax.java
@@ -0,0 +1,27 @@
+package com.atishay.practice.arrays;
+
+//Asked in VmWare Interview Question.
+//Runtime Complexity: O(n)
+//Space complexity: O(1)
+//Any pair whose sum is maximum.
+public class AnyPairSumMax {
+	public static void main(String[] args) {
+		int[] arr = { 1, 7, 5, 3, 0, 0, 2 };
+		int maxFirst = 0;
+		int maxSecond = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (maxFirst < arr[i]) {
+				maxSecond = maxFirst;
+				maxFirst = arr[i];
+			} else if (maxSecond < arr[i]) {
+				maxSecond = arr[i];
+			}
+		}
+
+		System.out.println("MaxFirst: " + maxFirst);
+		System.out.println("MaxSecond: " + maxSecond);
+
+		System.out.println("Pair of max sum: " + (maxFirst + maxSecond));
+	}
+}
