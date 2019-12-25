d3d38e4ef111e7b713918f03ed68a6fb195889e2
Atishay <Atishay@192.168.2.3>
2019-12-25 23:21:30 +0530

    Added Runtime and Space Complexity.

--- src/com/atishay/practice/arrays/ArrayRotationLeft.java
+++ src/com/atishay/practice/arrays/ArrayRotationLeft.java
@@ -1,31 +1,25 @@
 package com.atishay.practice.arrays;
 
-//This was the first time solution.
-//A Poor Solution.
+import java.util.Arrays;
+
+//Runtime Complexity: O(n * no. of times to shift)
+//Space Complexity: O(1)
 public class ArrayRotationLeft {
 	public static void main(String[] args) {
-		int tempLast = 0;
-		int tempCurrent = 0;
 
 		int[] arr = { 1, 2, 3, 4 };
 		// Array Should be rotated 2 times.
 		int n = 2;
 		int j = 0;
-		// no of times we have to shift.
 		while (j < n) {
-			for (int i = arr.length - 2; i >= 0; i--) {
-				if (i == arr.length - 2) {
-					tempLast = arr[i];
-					arr[i] = arr[i + 1];
-				} else {
-					tempCurrent = arr[i];
-					arr[i] = tempLast;
-					tempLast = tempCurrent;
-				}
+			int temp = arr[0];
+			for (int i = 0; i < arr.length - 1; i++) {
+				arr[i] = arr[i + 1];
 			}
-			arr[arr.length - 1] = tempLast;
-
+			arr[arr.length - 1] = temp;
 			j++;
 		}
+
+		Arrays.stream(arr).forEach(System.out::print);
 	}
 }
