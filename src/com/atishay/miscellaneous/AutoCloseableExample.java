6da9295151c1af41e355ccd91b25688b2db7ef4b
atishay007 <atishayjain23@gmail.com>
2020-02-16 12:43:48 +0530

    Use of Closeable and AutoCloseable Interface.

---
+++ src/com/atishay/miscellaneous/AutoCloseableExample.java
@@ -0,0 +1,29 @@
+package com.atishay.miscellaneous;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class AutoCloseableExample {
+
+	public static void main(String[] args) throws Exception {
+		try (TestMe2 testMe = new TestMe2()) {
+			System.out.println("Working with TestMe");
+		}
+	}
+}
+
+class TestMe implements AutoCloseable {
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("Resources are getting AutoClosed using AutCloseable");
+	}
+}
+
+class TestMe2 implements Closeable {
+//It also has same functionality as AutoCloseable.
+	@Override
+	public void close() throws IOException {
+		System.out.println("Resources are getting AutoClosed implementing Closesable interface");
+	}
+}
